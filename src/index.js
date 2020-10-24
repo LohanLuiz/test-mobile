e61f7ec638de67557cfbfea23e7f60d24db126c7
Lohan Luiz <lohanluiz12@gmail.com>
2020-10-23 22:48:12 -0400

    FlatList test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { View, FlatList, Text, StyleSheet, StatusBar } from 'react-native'
 
 import api from './services/api';
 
@@ -16,9 +16,15 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack!</Text>
-      </View>
+
+      <FlatList 
+        style={styles.container}
+        data={projects}
+        keyExtractor={project => project.id}
+        renderItem={({ item: project }) => (
+          <Text style={styles.project}>{project.title}</Text>
+        )}
+      />
     </>
   );
 }
@@ -27,13 +33,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 
-  title: {
+  project: {
     color: '#FFF',
-    fontSize: 32,
-    fontWeight: 'bold',
+    fontSize: 120,
   },
 });
